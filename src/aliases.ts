2fed6b29ce9fc114612d930ecd7e1d3ebfc0f87b
Blake Regalia <blake.regalia@gmail.com>
2024-10-23 18:54:55 -0700

    fix: subtle

--- src/aliases.ts
+++ src/aliases.ts
@@ -1,13 +1,13 @@
 
-export const subtle_decrypt = crypto.subtle.decrypt
-export const subtle_derive_bits = crypto.subtle.deriveBits;
-export const subtle_derive_key = crypto.subtle.deriveKey;
-export const subtle_digest = crypto.subtle.digest;
-export const subtle_encrypt = crypto.subtle.encrypt;
-export const subtle_export_key = crypto.subtle.exportKey;
-export const subtle_generate_key = crypto.subtle.generateKey;
-export const subtle_import_key = crypto.subtle.importKey;
-export const subtle_sign = crypto.subtle.sign;
-export const subtle_unwrap_key = crypto.subtle.unwrapKey;
-export const subtle_verify = crypto.subtle.verify;
-export const subtle_wrap_key = crypto.subtle.wrapKey;
+export const subtle_decrypt = (...a: Parameters<SubtleCrypto['decrypt']>): ReturnType<SubtleCrypto['decrypt']> => crypto.subtle.decrypt(...a);
+export const subtle_derive_bits = (...a: Parameters<SubtleCrypto['deriveBits']>): ReturnType<SubtleCrypto['deriveBits']> => crypto.subtle.deriveBits(...a);
+export const subtle_derive_key = (...a: Parameters<SubtleCrypto['deriveKey']>): ReturnType<SubtleCrypto['deriveKey']> => crypto.subtle.deriveKey(...a);
+export const subtle_digest = (...a: Parameters<SubtleCrypto['digest']>): ReturnType<SubtleCrypto['digest']> => crypto.subtle.digest(...a);
+export const subtle_encrypt = (...a: Parameters<SubtleCrypto['encrypt']>): ReturnType<SubtleCrypto['encrypt']> => crypto.subtle.encrypt(...a);
+export const subtle_export_key = (...a: Parameters<SubtleCrypto['exportKey']>): ReturnType<SubtleCrypto['exportKey']> => crypto.subtle.exportKey(...a);
+export const subtle_generate_key = (...a: Parameters<SubtleCrypto['generateKey']>): ReturnType<SubtleCrypto['generateKey']> => crypto.subtle.generateKey(...a);
+export const subtle_import_key = (...a: Parameters<SubtleCrypto['importKey']>): ReturnType<SubtleCrypto['importKey']> => crypto.subtle.importKey(...a);
+export const subtle_sign = (...a: Parameters<SubtleCrypto['sign']>): ReturnType<SubtleCrypto['sign']> => crypto.subtle.sign(...a);
+export const subtle_unwrap_key = (...a: Parameters<SubtleCrypto['unwrapKey']>): ReturnType<SubtleCrypto['unwrapKey']> => crypto.subtle.unwrapKey(...a);
+export const subtle_verify = (...a: Parameters<SubtleCrypto['verify']>): ReturnType<SubtleCrypto['verify']> => crypto.subtle.verify(...a);
+export const subtle_wrap_key = (...a: Parameters<SubtleCrypto['wrapKey']>): ReturnType<SubtleCrypto['wrapKey']> => crypto.subtle.wrapKey(...a);
