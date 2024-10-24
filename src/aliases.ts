2857acac6246a2364dd1257f2be1723fbeb15a71
Blake Regalia <blake.regalia@gmail.com>
2024-10-23 17:56:39 -0700

    feat: aliases

---
+++ src/aliases.ts
@@ -0,0 +1,13 @@
+
+export const subtle_decrypt = crypto.subtle.decrypt
+export const subtle_derive_bits = crypto.subtle.deriveBits;
+export const subtle_derive_key = crypto.subtle.deriveKey;
+export const subtle_digest = crypto.subtle.digest;
+export const subtle_encrypt = crypto.subtle.encrypt;
+export const subtle_export_key = crypto.subtle.exportKey;
+export const subtle_generate_key = crypto.subtle.generateKey;
+export const subtle_import_key = crypto.subtle.importKey;
+export const subtle_sign = crypto.subtle.sign;
+export const subtle_unwrap_key = crypto.subtle.unwrapKey;
+export const subtle_verify = crypto.subtle.verify;
+export const subtle_wrap_key = crypto.subtle.wrapKey;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+export * from './aliases.js';
 export * from './async.js';
 export * from './belt.js';
 export * from './cbor.js';
