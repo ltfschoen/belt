b827d1a2da5dd260f949862a409ab5609316a46e
Blake Regalia <blake.regalia@gmail.com>
2024-10-25 19:01:47 -0700

    fix: export mutex pool

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ export * from './async.js';
 export * from './belt.js';
 export * from './cbor.js';
 export * from './data.js';
+export * from './mutex-pool.js';
 export * from './strings.js';
 export * from './types.js';
